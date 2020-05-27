ad0ab72a500d3c9148a70b14ec6252f65b25549c
WRHerbert <will@wrherbert.com>
2020-05-26 19:19:55 -0700

    Update splash page to show coming soon (#3)
    * Update splash page to show coming soon
    
    * Updates tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Time to Watch header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(getByText("Time to Watch")).toBeInTheDocument();
+});
+
+test('renders Coming soon paragraph', () => {
+  const { getByText } = render(<App />);
+  expect(getByText("Coming soon")).toBeInTheDocument();
 });
